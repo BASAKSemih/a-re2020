e30a84748b294c579d5bea5df1cf14b3dd6c0e9b
BASAK Semih <semihbasak25@gmail.com>
2021-12-13 20:41:30 +0100

    feature: homePage test

---
+++ tests/Home/HomeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Home;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+
+
+final class HomeTest extends WebTestCase
+{
+    public function testShowHomePage(): void
+    {
+        $client = static::createClient();
+        /** @var RouterInterface $router */
+        $router = $client->getContainer()->get('router');
+        $crawler = $client->request(Request::METHOD_GET, $router->generate('homePage'));
+        self::assertRouteSame('homePage');
+    }
+
+}
