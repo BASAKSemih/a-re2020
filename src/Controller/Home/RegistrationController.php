7811c534a70be7c0345cf46cdc87f14c04289e85
BASAK Semih <semihbasak25@gmail.com>
2021-12-15 20:34:31 +0100

    user registration

---
+++ src/Controller/Home/RegistrationController.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller\Home;
+
+use App\Entity\User;
+use App\Form\UserType;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class RegistrationController extends AbstractController
+{
+    public function __construct(
+        protected EntityManagerInterface $entityManager,
+        protected UserRepository $userRepository,
+        protected UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
+    #[Route('/inscription', name: 'security_register')]
+    public function register(Request $request): Response
+    {
+        if ($this->getUser()) {
+            $this->addFlash('warning', 'Vous êtes connecter, vous ne pouvez pas vous inscrire');
+
+            return $this->redirectToRoute('homePage');
+        }
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $checkEmail = $this->userRepository->findOneByEmail($user->getEmail());
+            if (!$checkEmail) {
+                $passwordHash = $this->passwordHasher->hashPassword($user, $user->getPassword());
+                $user->setPassword($passwordHash);
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                $this->addFlash('success', 'Votre compte à bien été crée');
+
+                return $this->redirectToRoute('homePage');
+            }
+            $form = $this->createForm(UserType::class, $user)->handleRequest($request);
+            $this->addFlash('warning', 'Cette adresse email est déjà utiliser');
+
+            return $this->render('security/register.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+
+        return $this->render('security/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
