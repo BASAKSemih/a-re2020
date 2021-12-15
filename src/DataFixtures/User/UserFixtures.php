f7925ec60b1220b3c5752a34b89fd8194873ce2c
BASAK Semih <semihbasak25@gmail.com>
2021-12-15 20:42:02 +0100

    User Fixtures & Database user migration

---
+++ src/DataFixtures/User/UserFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataFixtures\User;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+final class UserFixtures extends Fixture
+{
+    public function __construct(private UserPasswordHasherInterface $userPasswordHasher)
+    {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        for ($index = 1; $index <= 10; ++$index) {
+            $user = new User();
+            $user
+                ->setEmail(sprintf('user+%d@email.com', $index))
+                ->setFirstName(sprintf('firstname+%d', $index))
+                ->setLastName(sprintf('firstname+%d', $index))
+                ->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
+            $manager->persist($user);
+        }
+        $manager->flush();
+    }
+}
