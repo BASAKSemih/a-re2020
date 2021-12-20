f047d0ca7f0af51ab593210ec8c7e9f9abcd45b3
BASAK Semih <semihbasak25@gmail.com>
2021-12-20 15:39:14 +0100

    test secondaryHeading create

--- src/Controller/User/Project/SecondaryHeading/SecondaryHeadingController.php
+++ src/Controller/User/Project/SecondaryHeading/SecondaryHeadingController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route(name: 'secondaryHeading')]
+#[Route(name: 'secondaryHeading_')]
 class SecondaryHeadingController extends AbstractController
 {
     public function __construct(
