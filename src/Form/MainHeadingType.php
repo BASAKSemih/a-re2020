1a21085865924fb0138c38c34e60efa08a734bc0
BASAK Semih <semihbasak25@gmail.com>
2021-12-20 14:19:46 +0100

    mainHeading create test

--- src/Form/MainHeadingType.php
+++ src/Form/MainHeadingType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\MainHeading;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,12 +15,56 @@ class MainHeadingType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('systems')
-            ->add('location')
-            ->add('heatingAppliance')
-            ->add('information')
-            ->add('createdAt')
-            ->add('project')
+            ->add('systems', ChoiceType::class, [
+                'required' => true,
+                'label' => false,
+                'choices' => [
+                    'GAZ' => 'GAZ',
+                    "FIOUL" => "FIOUL",
+                    'PAC EAU-GLYCOLEE' => 'PAC EAU-GLYCOLEE',
+                    'PAC AIR-EAU' => 'PAC AIR-EAU',
+                    "PELLETS" => "PELLETS",
+                    'BUCHES' => 'BUCHES',
+                    "PAC AIR-AIR" => "PAC AIR-AIR",
+                    'PAC EAU-EAU' => 'PAC EAU-EAU',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ], ])
+            ->add('location', ChoiceType::class, [
+                'required' => true,
+                'label' => false,
+                'choices' => [
+                    'En volume chauffé' => 'En volume chauffé',
+                    "Hors volume chauffé = cave, garage, etc..." => "Hors volume chauffé = cave, garage, etc...",
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ], ])
+            ->add('heatingAppliance', ChoiceType::class, [
+                'required' => true,
+                'label' => false,
+                'choices' => [
+                    'Plancher chauffant' => 'Plancher chauffant',
+                    "Radiateur" => "Radiateur",
+                    'Poêle à bois + sèche serviette puissance 500W dans la salle de bain' => 'Poêle à bois + sèche serviette puissance 500W dans la salle de bain',
+                ],
+                'attr' => [
+                    'class' => 'form-control',
+                ], ])
+            ->add('information', TextareaType::class, [
+                'required' => true,
+                'label' => false,
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => false,
+                'attr' => [
+                    'class' => 'btn btn-lg btn-primary mt-2',
+                ],
+            ])
         ;
     }
 
