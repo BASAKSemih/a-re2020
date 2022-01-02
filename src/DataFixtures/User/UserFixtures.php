4868b8f276d24f7c6c2db47482701f6abe07275f
BASAK Semih <semihbasak25@gmail.com>
2022-01-02 14:05:41 +0100

    user need to verify his email to connect

--- src/DataFixtures/User/UserFixtures.php
+++ src/DataFixtures/User/UserFixtures.php
@@ -22,6 +22,7 @@ final class UserFixtures extends Fixture
             $user
                 ->setEmail(sprintf('user+%d@email.com', $index))
                 ->setFirstName(sprintf('firstname+%d', $index))
+                ->setIsVerified(true)
                 ->setLastName(sprintf('firstname+%d', $index))
                 ->setPassword($this->userPasswordHasher->hashPassword($user, 'password'));
             $user->setRoles(['ROLE_USER']);
