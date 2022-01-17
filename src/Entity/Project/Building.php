fe0a028924b14d7bbe23f8581bd5b72b422a0772
BASAK Semih <semihbasak25@gmail.com>
2022-01-17 11:18:33 +0100

    moving BuildingRepository.php

--- src/Entity/Project/Building.php
+++ src/Entity/Project/Building.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Entity\Project;
 
-use App\Repository\BuildingRepository;
+use App\Repository\Project\BuildingRepository;
 use DateTimeImmutable;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
