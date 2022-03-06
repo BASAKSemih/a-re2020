62853ee236968a76b57ce395a7cbff29990fd71f
ysoftzahrane <89729045+ysoftzahrane@users.noreply.github.com>
2022-03-06 10:10:20 +0100

    CamelCase

--- src/Controller/Home/confidentialiteController.php
+++ src/Controller/Home/confidentialiteController.php
@@ -8,9 +8,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-final class confidentialiteController extends AbstractController
+final class confidentController extends AbstractController
 {
-    #[Route('/confidentialite', name: 'confidentialitePage')]
+    #[Route('/confidentialite', name: 'confidentPage')]
     public function home(): Response
     {
         return $this->render('confidentialite/confidentialite.html.twig');
